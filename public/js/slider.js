9e0d5ebfe06cd632a4304b6652e58816fddad052
mattmannec <mattmannec@gmail.com>
2020-02-27 15:13:17 +0100

    corrected stylesheet heritage structure

--- public/js/slider.js
+++ public/js/slider.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1, 
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 3000,
         arrows: false,
     })
 });
