af08e1c4755f5531b5137e4d0bc55a040d6b1230
mattmannec <mattmannec@gmail.com>
2020-03-05 15:43:02 +0100

    container fluid

--- src/Form/WineType.php
+++ src/Form/WineType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Wine;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
 use Symfony\Component\Form\FormBuilderInterface;
